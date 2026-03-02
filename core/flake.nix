98482c7a023205f0113adc002a9543643992c1df
Hong Jiang <hjiang@users.noreply.github.com>
2026-03-02 14:25:49 +0800

    remove unnecessary comments

--- core/flake.nix
+++ core/flake.nix
@@ -10,10 +10,8 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        # Use erlang_27 to match .tool-versions: erlang 27.2
         beamPackages = pkgs.beam.packages.erlang_27;
         erlang = beamPackages.erlang;
-        # elixir_1_19 built against OTP 27, matching .tool-versions: elixir 1.19.0-otp-27
         elixir = beamPackages.elixir_1_19;
       in
       {
